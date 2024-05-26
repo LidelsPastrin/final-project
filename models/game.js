2441912454ea83f1eeb45a3c98480ef7c12f0cbf
LidelsPastrin <149607624+LidelsPastrin@users.noreply.github.com>
2024-05-26 14:51:41 +0300

    Мангусты со мной не дружат

---
+++ models/game.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+// Импорт модели для связывания 
+const categoryModel = require('./category');
+
+const gameSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  developer: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  link: {
+    type: String,
+    required: true
+  },
+  categories: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: categoryModel,
+  }],
+});
+
+module.exports = mongoose.model('game', gameSchema);
